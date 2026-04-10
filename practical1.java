6b8c2f806b3b7281ba4305a6250aa1ae52c38e6d
PIYUSH BIPIN VARULE <piyushvarule15@gmail.com>
2026-04-11 01:51:43 +0530

    Add practical1.java with basic data types example

---
+++ practical1.java
@@ -0,0 +1,19 @@
+package practical1;
+
+public class practical1 {
+	public static void main(String args[]) {
+		int age = 16;
+		float per= 95.65f;
+		boolean tf= true;
+		//non primitive datatypes example :- 
+		String name = "piyush bipin varule ";
+		int [] arg= {1,2,3,4,5,6};
+		System.out.println("age"+age);
+		System.out.println("per"+per);
+		System.out.println("tf"+tf);
+		System.out.println("name"+name);
+		System.out.println("arg"+arg);
+		
+	}
+
+}
